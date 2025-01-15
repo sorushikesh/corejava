3cb8b6c1b7edc1dfab0a8765f8195746c59a3f25
Rushikesh <sorushikesh07@gmail.com>
2025-01-15 20:19:21 +0530

    Initial commit

--- src/main/java/com/corejava/CorejavaApplication.java
+++ src/main/java/com/corejava/CorejavaApplication.java
@@ -1,13 +1,13 @@
-package com.study;
+package com.corejava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CoreJavaApplication {
+public class CorejavaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CoreJavaApplication.class, args);
+		SpringApplication.run(CorejavaApplication.class, args);
 	}
 
 }
